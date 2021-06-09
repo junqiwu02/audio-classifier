8f00e0ec23c74cb281317cd86c9927afef7204a5
junqiwu02 <junqiwu02@gmail.com>
2021-06-09 03:46:58 -0400

    working example

---
+++ extract.py
@@ -0,0 +1,11 @@
+# %%
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+import scipy.io.wavfile as wav
+# %%
+(rate,sig) = wav.read("dataset/male.wav")
+mfcc_feat = mfcc(sig,rate)
+fbank_feat = logfbank(sig,rate)
+
+print(fbank_feat[1:3,:])
+# %%
