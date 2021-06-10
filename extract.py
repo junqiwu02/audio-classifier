83eebc56e83c6f896183bf3ca88d3e3d492faa74
junqiwu02 <junqiwu02@gmail.com>
2021-06-10 13:48:03 -0400

    working librosa example, vggish is throwing an error

--- extract.py
+++ extract.py
@@ -2,10 +2,24 @@
 from python_speech_features import mfcc
 from python_speech_features import logfbank
 import scipy.io.wavfile as wav
+import librosa
+import matplotlib.pyplot as plt
+import librosa.display
 # %%
 (rate,sig) = wav.read("dataset/male.wav")
 mfcc_feat = mfcc(sig,rate)
 fbank_feat = logfbank(sig,rate)
 
 print(fbank_feat[1:3,:])
+# %%
+x, sr = librosa.load("dataset/male.wav")
+librosa.display.waveplot(x, sr=sr)
+# %%
+#display Spectrogram
+X = librosa.stft(x)
+Xdb = librosa.amplitude_to_db(abs(X))
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz') 
+#If to pring log of frequencies  
+librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar()
 # %%
