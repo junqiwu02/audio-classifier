967c75b20c610319fca1253f670d2f345c529fa7
junqiwu02 <junqiwu02@gmail.com>
2021-07-23 16:08:39 -0400

    reverted model, other ones didn't converge or collapsed

--- classify.py
+++ classify.py
@@ -3,7 +3,7 @@ from preprocess import *
 import analysis
 
 from tensorflow import keras
-from tensorflow.keras.layers import InputLayer, Bidirectional, LSTM, Dense, TimeDistributed
+from tensorflow.keras.layers import InputLayer, LSTM, Dense
 
 # %%
 # data = Preprocess()
@@ -16,7 +16,8 @@ X_train = pickle_load('./data/x_train_vggish.pkl')
 y_train = pickle_load('./data/y_train_vggish.pkl')
 
 # %%
-sample_weight = get_sample_weight(y_train)
+# sample_weight = get_sample_weight(y_train)
+X_train, y_train = resample(X_train, y_train)
 
 # %%
 y_train = one_hot(y_train)
@@ -24,8 +25,8 @@ y_train = one_hot(y_train)
 # %%
 model = keras.Sequential()
 model.add(InputLayer(input_shape=(None, 512)))
-model.add(Bidirectional(LSTM(300, return_sequences=True, activation="tanh", dropout=0.4)))
-model.add(Bidirectional(LSTM(300, activation="tanh", dropout=0.4)))
+model.add(LSTM(128, return_sequences=True, activation="tanh"))
+model.add(LSTM(128, activation="tanh"))
 model.add(Dense(7))
 print(model.summary())
 
@@ -37,7 +38,8 @@ model.compile(
 )
 
 # %%
-model.fit(X_train, y_train, sample_weight=sample_weight, batch_size=50, epochs=100)
+# model.fit(X_train, y_train, sample_weight=sample_weight, batch_size=50, epochs=100)
+model.fit(X_train, y_train, batch_size=64, epochs=100)
 
 # %%
 # X_test, y_test = data.get_data('./data/test_sent_emo.csv', './data/output_repeated_splits_test/wav')
